5dd832747a229d22160043c558a29f4ddf203cc3
Shelley Vohr <shelley.vohr@gmail.com>
2024-12-01 21:50:16 +0100

    Add day 1

---
+++ day_1/p1.js
@@ -0,0 +1,45 @@
+const fs = require('node:fs');
+
+function parseInput() {
+  const input = fs.readFileSync('./input/day_1.txt', 'utf8');
+
+  const one = [];
+  const two = [];
+  input.trim().split('\n').map((line) => {
+    const [a, b] = line.split('   ');
+    one.push(a);
+    two.push(b);
+  });
+  return { one, two };
+}
+
+const { one, two } = parseInput();
+
+// Part 1
+function getDistance(a, b) {
+  const sortedLeft = a.sort((a, b) => a - b);
+  const sortedRight = b.sort((a, b) => a - b);
+
+  return sortedLeft.reduce((acc, leftValue, index) => {
+    const rightValue = sortedRight[index];
+    return acc + Math.abs(rightValue - leftValue);
+  }, 0);
+}
+
+console.log(`Part 1 Answer: ${getDistance(one, two)}`);
+
+// Part 2
+function getSimilarityScore(one, two) {
+  const map = new Map();
+  two.map((value) => map.set(value, map.has(value) ? map.get(value) + 1 : 1));
+
+  let score = 0;
+  one.forEach(num => {
+    if (map.has(num)) {
+      score += map.get(num) * num;
+    }
+  });
+  return score;
+}
+
+console.log(`Part 2 Answer: ${getSimilarityScore(one, two)}`);
