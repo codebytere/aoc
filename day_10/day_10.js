70c1feb1c076ca5ce8d60372cbde681fd373cd1e
Shelley Vohr <shelley.vohr@gmail.com>
2024-12-11 10:07:51 +0000

    Clean up day 10

--- day_10/day_10.js
+++ day_10/day_10.js
@@ -17,7 +17,7 @@ const DIR_TO_COORD = {
   S: [0, -1],
 };
 
-function checkDirectionScore(grid, pos) {
+function checkDirectionScore(grid, pos, type) {
   let [x, y] = pos;
   const stack = [{ x, y, idx: 0 }];
   const seen = new Set();
@@ -26,8 +26,9 @@ function checkDirectionScore(grid, pos) {
   while (stack.length > 0) {
     const { x, y, idx } = stack.pop();
 
-    if (idx === 9 && !seen.has(`${x},${y}`)) {
-      seen.add(`${x},${y}`);
+    const shouldScore = type === 'score' ? !seen.has(`${x},${y}`) : true;
+    if (idx === 9 && shouldScore) {
+      if (type === 'score') seen.add(`${x},${y}`);
       score += 1;
       continue;
     }
@@ -47,61 +48,22 @@ function checkDirectionScore(grid, pos) {
   return score;
 }
 
-function calculateTrailHeadScores(grid) {
+function calculateTrailHeadValue(grid, type) {
   let totalScore = 0;
   for (let x = 0; x < grid.length; x++) {
     for (let y = 0; y < grid[x].length; y++) {
       if (grid[x][y] === 0) {
-        totalScore += checkDirectionScore(grid, [x, y]);
+        totalScore += checkDirectionScore(grid, [x, y], type);
       }
     }
   }
   return totalScore;
 }
 
-const score = calculateTrailHeadScores(input);
+// Part 1
+const score = calculateTrailHeadValue(input, 'score');
 console.log(`Part 1 Answer: ${score}`);
 
 // Part 2
-function checkDirectionRating(grid, pos) {
-  let [x, y] = pos;
-  const stack = [{ x, y, idx: 0 }];
-  let score = 0;
-
-  while (stack.length > 0) {
-    const { x, y, idx } = stack.pop();
-
-    if (idx === 9) {
-      score += 1;
-      continue;
-    }
-
-    for (const dir of ['N', 'E', 'S', 'W']) {
-      const [dx, dy] = DIR_TO_COORD[dir];
-      const nx = x + dx;
-      const ny = y + dy;
-      const next = grid[nx]?.[ny];
-
-      if (next === idx + 1) {
-        stack.push({ x: nx, y: ny, idx: idx + 1 });
-      }
-    }
-  }
-
-  return score;
-}
-
-function calculateTrailHeadRatings(grid) {
-  let totalScore = 0;
-  for (let x = 0; x < grid.length; x++) {
-    for (let y = 0; y < grid[x].length; y++) {
-      if (grid[x][y] === 0) {
-        totalScore += checkDirectionRating(grid, [x, y]);
-      }
-    }
-  }
-  return totalScore;
-}
-
-const rating = calculateTrailHeadRatings(input);
+const rating = calculateTrailHeadValue(input, 'rating');
 console.log(`Part2  Answer: ${rating}`);
